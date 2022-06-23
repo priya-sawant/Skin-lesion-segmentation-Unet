7764fe54aad0d09ea7d5aff4e9b804863e290f62
Priyanka Sawant <48020934+priya-sawant@users.noreply.github.com>
2022-06-23 19:48:54 +0100

    Add files via upload

---
+++ train.py
@@ -0,0 +1,142 @@
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+import numpy as np
+import cv2
+from glob import glob
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from tensorflow.keras.callbacks import ModelCheckpoint, CSVLogger, ReduceLROnPlateau, EarlyStopping, TensorBoard
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.metrics import Recall, Precision
+from model import build_unet
+from metrics import dice_coef, iou
+
+H = 256
+W = 256
+
+def create_dir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+def shuffling(x, y):
+    x, y = shuffle(x, y, random_state=42)
+    return x, y
+
+def load_data(dataset_path, split=0.2):
+
+    images = sorted(glob(os.path.join(dataset_path, "ISIC2018_Task1-2_Training_Input", "*.jpg")))
+    masks = sorted(glob(os.path.join(dataset_path, "ISIC2018_Task1_Training_GroundTruth", "*.png")))
+
+
+    test_size = int(len(images) * split)
+
+    train_x, valid_x = train_test_split(images, test_size=test_size, random_state=42)
+    train_y, valid_y = train_test_split(masks, test_size=test_size, random_state=42)
+
+    train_x, test_x = train_test_split(train_x, test_size=test_size, random_state=42)
+    train_y, test_y = train_test_split(train_y, test_size=test_size, random_state=42)
+
+    return (train_x, train_y), (valid_x, valid_y), (test_x, test_y)
+
+def read_image(path):
+    path = path.decode()
+    x = cv2.imread(path, cv2.IMREAD_COLOR)  ## (H, W, 3) to read as rgb
+    x = cv2.resize(x, (W, H))
+    x = x/255.0
+    x = x.astype(np.float32)
+    return x                                ## (256, 256, 3) returns image array
+
+def read_mask(path):
+    path = path.decode()
+    x = cv2.imread(path, cv2.IMREAD_GRAYSCALE)  ## (H, W) to read img as grayscale
+    x = cv2.resize(x, (W, H))
+    x = x/255.0
+    x = x.astype(np.float32)                    ## (256, 256)
+    x = np.expand_dims(x, axis=-1)              ## (256, 256, 1) here we added channel
+    return x
+
+def tf_parse(x, y):
+    def _parse(x, y):
+        x = read_image(x)
+        y = read_mask(y)
+        return x, y                                #return numpy array ,returns float 32
+
+    x, y = tf.numpy_function(_parse, [x, y], [tf.float32, tf.float32])
+    x.set_shape([H, W, 3])
+    y.set_shape([H, W, 1])
+    return x, y
+
+def tf_dataset(X, Y, batch):                       #X= list of images Y= list of masks
+    dataset = tf.data.Dataset.from_tensor_slices((X, Y))
+    dataset = dataset.map(tf_parse)                 #to read the path in tensors
+    dataset = dataset.batch(batch)                  #provide batch_size
+    dataset = dataset.prefetch(10)
+    return dataset
+
+if __name__ == "__main__":
+    """ Seeding """
+    np.random.seed(42)
+    tf.random.set_seed(42)
+
+    """ Folder for saving data """
+    create_dir("files")
+
+    """ Hyperparameters """
+    batch_size = 4
+    lr = 1e-4 ## (0.0001)
+    num_epoch = 5
+
+    """create path to save weights"""
+    model_path = "files/model.h5"
+
+    """create path to save all metrics"""
+    csv_path = "files/data.csv"
+
+    """ Dataset : 60/20/20 """
+    dataset_path = "C:/Users/Priyanka Sawant/Downloads/ISIC Challenge/"
+    (train_x, train_y), (valid_x, valid_y), (test_x, test_y) = load_data(dataset_path)
+
+    print(f"Train: {len(train_x)} - {len(train_y)}")
+    print(f"Valid: {len(valid_x)} - {len(valid_y)}")
+    print(f"Test: {len(test_x)} - {len(test_y)}")
+
+
+
+    train_dataset = tf_dataset(train_x, train_y, batch_size)
+    valid_dataset = tf_dataset(valid_x, valid_y, batch_size)
+
+    #to calculates batches- 1558//4=389
+    train_steps = len(train_x)//batch_size
+    valid_steps = len(valid_x)//batch_size
+
+    #if remainder!=0
+    if len(train_x) % batch_size != 0:
+        train_steps += 1
+
+    if len(valid_x) % batch_size != 0:
+        valid_steps += 1
+
+    """ Model """
+    model = build_unet((H, W, 3))
+    metrics = [dice_coef, iou, Recall(), Precision()]
+    model.compile(loss="binary_crossentropy", optimizer=Adam(lr), metrics=metrics)
+    model.summary()
+
+    #will call callbacks at training
+    callbacks = [
+        ModelCheckpoint(model_path, verbose=1, save_best_only=True),            #to save weightfile while training
+        ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=5, min_lr=1e-7, verbose=1),
+        CSVLogger(csv_path),                                                    #to log all the data while training
+        TensorBoard(),                                                          #for visualization
+        EarlyStopping(monitor='val_loss', patience=20, restore_best_weights=False) #to reduce overfitting
+    ]
+
+    model.fit(
+        train_dataset,
+        epochs=num_epoch,
+        validation_data=valid_dataset,
+        steps_per_epoch=train_steps,
+        validation_steps=valid_steps,
+        callbacks=callbacks
+    )
